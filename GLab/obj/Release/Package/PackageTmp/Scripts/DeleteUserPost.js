fbfd2841ecde20957982cabe4a161918676e7ee5
GiorgiKhutsishvili <34691385+GiorgiKhutsishvili@users.noreply.github.com>
2017-12-27 23:40:20 +0400

    commit changes

---
+++ GLab/obj/Release/Package/PackageTmp/Scripts/DeleteUserPost.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+    var firstTr;
+    var Id;
+
+    //$('#tbl tr').click(function () {
+
+    //});
+
+    $('.command-remove').on('click', function () {
+
+        firstTr = $(this).closest('tr');
+        Id = firstTr.find('td:first').html();
+        
+
+        $('#confirmdelete').on('click', function () {
+            //alert(Id);
+            //alert("daechira");
+            $.ajax({
+                url: '/Admin/DeleteUserPost',
+                method: 'POST',
+                data: { 'Id': Id },
+                success: function (response) {
+                    if (response == "DeleteSucceeded") {
+                       
+                        $('.PostedData').trigger('click');
+                        window.location.href = "/Admin/AddPost";
+                    }
+                }
+
+            });
+        });
+    });
+
+});
